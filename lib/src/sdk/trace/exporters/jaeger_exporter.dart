9887be0d1195b31aa50683f5a7e15f389bb2839c
ikorostelev <blacqocelot@gmail.com>
2023-03-22 15:23:23 +0600

    jaeger exporter

--- lib/src/sdk/trace/exporters/jaeger_exporter.dart
+++ lib/src/sdk/trace/exporters/jaeger_exporter.dart
@@ -27,7 +27,7 @@ class JaegerExporter implements api.SpanExporter {
     final spansList = spansToMap(spans);
     Logger().i({
       'data': spansList,
-      'converted': convert.jsonEncode(spansList),
+      // 'converted': convert.jsonEncode(spansList),
     });
 
     // client.post(
